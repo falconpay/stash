d68b55854a32b91349e325e6a4b8c81ac4036cea
abdegenius <johnnieabijah2000@gmail.com>
2026-06-12 12:22:03 +0100

    feat:updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,10 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  minimumScale: 1,
   maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
   themeColor: "#0E1117",
 };
 
